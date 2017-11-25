16e0b15411edb4d6d9dfe138ef398d277835f9c9
jgradt <>
2017-11-24 18:55:54 -0600

    some cleanup;  remove systemjs

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -8,9 +8,8 @@ export class App {
     static main() {
         //do something here
         document.getElementById("content").innerText = 'Hello Typescript class';
-        Observable.range(1,15).subscribe(val=>console.log(val));
+        Observable.range(1,3).subscribe(val=>console.log(val));
         console.log("app main");
-        //alert('hello');
     }
     
 }
